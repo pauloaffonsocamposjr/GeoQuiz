0b2a0af1dac3c70b891a35ab548663e375d2c452
Paulinho <pauloaffonsocamposjr@gmail.com>
2022-01-31 16:33:59 -0300

    Fixed rotation bug. Implemented ViewModels and Saved Instance States.

--- app/src/main/java/android/bignerdranch/geoquiz/MainActivity.kt
+++ app/src/main/java/android/bignerdranch/geoquiz/MainActivity.kt
@@ -1,41 +1,43 @@
 package android.bignerdranch.geoquiz
-
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+
+private const val KEY_INDEX = "index"
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
-    private lateinit var falseButton : Button
-    private lateinit var nextButton : ImageButton
-    private lateinit var prevButton : ImageButton
-    private lateinit var questionTextView : TextView
-    private var numQuestionsAnswered : Double = 0.0
-    private var numCorrectAnswers : Double = 0.0
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextView: TextView
+    private var numQuestionsAnswered: Double = 0.0
+    private var numCorrectAnswers: Double = 0.0
 
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true))
-
-    private var currentIndex = 0
+    private val quizViewModel: QuizViewModel by lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle? called")
         setContentView(R.layout.activity_main)
+
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -52,22 +54,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
             trueButton.isClickable = true
             falseButton.isClickable = true
         }
 
-        prevButton.setOnClickListener { // Creating a Previous button challenge
-            if (currentIndex > 0) {
-                currentIndex = (currentIndex - 1) % questionBank.size
-                updateQuestion()
-            }
+        prevButton.setOnClickListener { // Creating a Previous button Challenge
+            quizViewModel.moveToPrev()
+            updateQuestion()
+            trueButton.isClickable = true
+            falseButton.isClickable = true
         }
 
         questionTextView.setOnClickListener { // Clicking on the text view to change question Challenge
-            currentIndex = (currentIndex + 1) % questionBank.size
+            quizViewModel.moveToNext()
             updateQuestion()
+            trueButton.isClickable = true
+            falseButton.isClickable = true
         }
 
         updateQuestion()
@@ -77,25 +81,35 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         Log.d(TAG, "onStart() called")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume() called")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause() called")
     }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "onSaveInstanceState")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
@@ -103,23 +117,24 @@ class MainActivity : AppCompatActivity() {
         trueButton.isClickable = false
         falseButton.isClickable = false
         numQuestionsAnswered += 1
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         var messageResId = ""
         if (userAnswer == correctAnswer) {
             messageResId = "Correct!"
             numCorrectAnswers += 1
-        }
-        else {
+        } else {
             messageResId = "Incorrect!"
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
 
-        if (numQuestionsAnswered >= questionBank.size) {
-            val percentageOfCorrectAnswers = (numCorrectAnswers / numQuestionsAnswered) * 100
-            val messageCorrectPercentage = "$percentageOfCorrectAnswers%"
-            Toast.makeText(this, messageCorrectPercentage, Toast.LENGTH_LONG).show()
-            numCorrectAnswers = 0.0
-            numQuestionsAnswered = 0.0
+        if (numQuestionsAnswered >= quizViewModel.getQuestionBankSize()) {
+            if (numQuestionsAnswered >= quizViewModel.getQuestionBankSize()) {
+                val percentageOfCorrectAnswers = (numCorrectAnswers / numQuestionsAnswered) * 100
+                val messageCorrectPercentage = "$percentageOfCorrectAnswers%"
+                Toast.makeText(this, messageCorrectPercentage, Toast.LENGTH_LONG).show()
+                numCorrectAnswers = 0.0
+                numQuestionsAnswered = 0.0
+            }
         }
     }
 }
