58a6d57a1f4976d22ddeab764fee48d972d544aa
Paulinho <pauloaffonsocamposjr@gmail.com>
2022-01-31 09:03:42 -0300

    Made impossible to answer the same question twice, added percentage of correct answers after answering the entire question bank.

--- app/src/main/java/android/bignerdranch/geoquiz/MainActivity.kt
+++ app/src/main/java/android/bignerdranch/geoquiz/MainActivity.kt
@@ -2,11 +2,14 @@ package android.bignerdranch.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton : ImageButton
     private lateinit var prevButton : ImageButton
     private lateinit var questionTextView : TextView
+    private var numQuestionsAnswered : Double = 0.0
+    private var numCorrectAnswers : Double = 0.0
 
     private val questionBank = listOf(
         Question(R.string.question_australia, true),
@@ -29,6 +34,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle? called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -48,6 +54,8 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
+            trueButton.isClickable = true
+            falseButton.isClickable = true
         }
 
         prevButton.setOnClickListener { // Creating a Previous button challenge
@@ -65,20 +73,53 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
+        trueButton.isClickable = false
+        falseButton.isClickable = false
+        numQuestionsAnswered += 1
         val correctAnswer = questionBank[currentIndex].answer
-
-        val messageResId = if (userAnswer == correctAnswer) {
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        var messageResId = ""
+        if (userAnswer == correctAnswer) {
+            messageResId = "Correct!"
+            numCorrectAnswers += 1
         }
-
+        else {
+            messageResId = "Incorrect!"
+        }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+
+        if (numQuestionsAnswered >= questionBank.size) {
+            val percentageOfCorrectAnswers = (numCorrectAnswers / numQuestionsAnswered) * 100
+            val messageCorrectPercentage = "$percentageOfCorrectAnswers%"
+            Toast.makeText(this, messageCorrectPercentage, Toast.LENGTH_LONG).show()
+            numCorrectAnswers = 0.0
+            numQuestionsAnswered = 0.0
+        }
     }
 }
