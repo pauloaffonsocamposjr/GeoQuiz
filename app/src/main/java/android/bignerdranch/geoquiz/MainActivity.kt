a89c182197aed9e6152f17fb76a4414e3abc7cc5
Paulinho <pauloaffonsocamposjr@gmail.com>
2022-01-31 08:04:45 -0300

    Added previous button, changed prevButton and nextButton to ImageButtons

--- app/src/main/java/android/bignerdranch/geoquiz/MainActivity.kt
+++ app/src/main/java/android/bignerdranch/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
@@ -11,7 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton: Button
     private lateinit var falseButton : Button
-    private lateinit var nextButton : Button
+    private lateinit var nextButton : ImageButton
+    private lateinit var prevButton : ImageButton
     private lateinit var questionTextView : TextView
 
     private val questionBank = listOf(
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -43,6 +46,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+
+        prevButton.setOnClickListener { // Creating a Previous button challenge
+            if (currentIndex > 0) {
+                currentIndex = (currentIndex - 1) % questionBank.size
+                updateQuestion()
+            }
+        }
+
+        questionTextView.setOnClickListener { // Clicking on the text view to change question Challenge
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
